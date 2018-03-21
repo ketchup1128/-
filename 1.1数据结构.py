61160007921632e4cea2b14e60f77a8a5bc342fc
ketchup1128 <35157516+ketchup1128@users.noreply.github.com>
2018-03-21 10:37:23 +0800

    1.1python数据类型

---
+++ 1.1数据结构.py
@@ -0,0 +1,23 @@
+# coding=UTF-8
+a = 10
+b = 10.5
+c = True
+d = None
+e = 'liumin'
+f = input("please input a num: ")
+print(type(a), a)
+print(type(b), b)
+print(type(c), c)
+print(type(d), d)
+print(type(e), e)
+print(type(f), f)
+
+# 在python3中通过input函数得到的值是'str'类型
+# 以下是程序的输出结果：
+# please input a num: 5
+# <class 'int'> 10
+# <class 'float'> 10.5
+# <class 'bool'> True
+# <class 'NoneType'> None
+# <class 'str'> liumin
+# <class 'str'> 5
